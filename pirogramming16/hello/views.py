0e37b950c8ed6d82827095fb97fc9940c925b42e
KYBee <kyb3634@naver.com>
2022-01-10 22:53:04 +0900

    6. make hello page

--- pirogramming16/hello/views.py
+++ pirogramming16/hello/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def hello(request):
+
+    context = {
+        "name" : "여러분들의 이름을 적으세요"
+    }
+
+    return render(request, template_name="hello.html", context=context)
